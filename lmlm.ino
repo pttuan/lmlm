e4e49224ab78d0019a4099a0b26cdb3b420f8489
Tuan Phan <pttuan@gmail.com>
2023-10-05 11:33:17 -0700

    Replaced MPU6050 with BNO055
    This version works nicely with Euler angle returned from fusion of
    BNO055
    
    Signed-off-by: Tuan Phan <pttuan@gmail.com>

--- lmlm.ino
+++ lmlm.ino
@@ -22,67 +22,48 @@ THE SOFTWARE.
 ===============================================
 */
 
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
 #include "analogWave.h"
 #include "Wire.h"
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BNO055.h"
 #include <WDT.h>
 #include <ArduinoBLE.h>
 #include <HardwareBLESerial.h>
 
 #define WDT_TIMEOUT               5000
 
-#define MOTION_DURATION_CHECK     200
-#define MOTION_SENSITIVE          20
-#define MOTION_AVERAGE            5
-#define WEIGHT_MOVING             20
-
-#define ACC_SENSOR_LIFE_DURATION  3600000 // 1 hour
-
-#define PRESSURE_RATIO            (float)3.5
 #define PRESSURE_CUT_LOW          2000
 #define PRESSURE_REFILL           6000
 #define PRESSURE_CUT_HIGH         9000
 #define PRESSURE_VOLTAGE_TOP      40000
-#define PRESSURE_VOLTAGE_BOTTOM   64000
-#define START_MOVE_PRESSURE       (PRESSURE_CUT_LOW - 1500) //minues due to error move reading from begin
-#define MOTOR_DELAY_UPDATE        100
+#define PRESSURE_VOLTAGE_BOTTOM   65000
+#define PRESSURE_MIN_STEP         100
+#define ANGLE_CUT_LOW             0
+#define ANGLE_CUT_HIGH            50
+#define ANGLE_NOISE_FILTER        100
 
-#define VCC_SENSOR_PIN            D0
 #define REFILL_PIN                D2
 #define BREW_PIN_IN               D3
 #define MOTOR_ENABLE_OUT          D4
 #define BREW_PIN_OUT              D5
 #define SWITCH_IN                 D6
 
+#define BNO055_SAMPLERATE_DELAY_MS  10 //how often to read data from the board
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29, &Wire1);
+
 HardwareBLESerial &bleSerial = HardwareBLESerial::getInstance();
 
-// MPU control/status vars
-MPU6050 mpu;
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-bool acc_sensor_ready;
-unsigned long acc_sensor_enable_timer;
+bool sensor_ready;
+unsigned long sensor_read_last_time;
 
 // DAC vars
 uint16_t dac_sample[24];
 analogWave wave(DAC, dac_sample, 24, 0);
 
 // Linea mini vars
-uint32_t motion_detect_timer;
-int32_t last_delta;
-int last_move; // -1->left, 0-neutral, 1 -> right
-int32_t acc_total;
 int32_t current_pressure;
 int32_t last_pressure;
-bool motion_monitor;
 
 //  pin status
 int refill_state;
@@ -92,160 +73,132 @@ int switch_state;
 void motor_analog_out(int32_t pressure);
 void(* resetFunc) (void) = 0;
 
-int get_temp()
+void sensor_status(void)
 {
-  if (!acc_sensor_ready)
-    return 0;
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
 
-  return (mpu.getTemperature() / 340 + 36.5);
+  /* Display the results in the Serial Monitor */
+  bleSerial.println("");
+  bleSerial.print("Current mode: 0x");
+  bleSerial.println(bno.getMode(), HEX);
+  bleSerial.print("System Status: 0x");
+  bleSerial.println(system_status, HEX);
+  bleSerial.print("Self Test:     0x");
+  bleSerial.println(self_test_results, HEX);
+  bleSerial.print("System Error:  0x");
+  bleSerial.println(system_error, HEX);
+  bleSerial.println("");
 }
 
-void enable_acc_sensor(bool enable)
+void sensor_cal_status(void)
 {
-  if (enable) {
-    digitalWrite(VCC_SENSOR_PIN, LOW);
-    delay(500);
-    digitalWrite(VCC_SENSOR_PIN, HIGH);
-    delay(1000);
-    acc_sensor_enable_timer = millis();
-  } else {
-    acc_sensor_ready = false;
-    Wire1.end();
-    delay(10);
-    digitalWrite(VCC_SENSOR_PIN, LOW);
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  /* The data should be ignored until the system calibration is > 0 */
+  if (!system)
+  {
+    bleSerial.print("! ");
   }
+
+  /* Display the individual values */
+  bleSerial.print("Sys:");
+  bleSerial.print(system, DEC);
+  bleSerial.print(" G:");
+  bleSerial.print(gyro, DEC);
+  bleSerial.print(" A:");
+  bleSerial.print(accel, DEC);
+  bleSerial.print(" M:");
+  bleSerial.println(mag, DEC);
 }
 
-void setup_acc_sensor()
+void setup_sensor()
 {
-  bool test_conn;
+  sensor_ready = false;
+  sensor_read_last_time = 0;
 
-  Wire1.begin();
+  if (!bno.begin(OPERATION_MODE_IMUPLUS))
+  {
+    bleSerial.print("No BNO055 detected");
+    return;
+  }
   Wire1.setClock(400000);
-  delay(10);
+  delay(1000);
+  bno.setExtCrystalUse(true);
+  delay(100);
 
-  bleSerial.println(F("Initializing MPU6050..."));
-  mpu.initialize();
-  mpu.setTempSensorEnabled(true);
-
-  // verify connection
-  bleSerial.println(F("Testing device connections..."));
-  test_conn = mpu.testConnection();
-  bleSerial.println(test_conn ?
-                  F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  // wait for ready
-  if (!test_conn) {
-    goto failed;
-  }
-
-  // load and configure the DMP
-  bleSerial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(126);
-  mpu.setYGyroOffset(-26);
-  mpu.setZGyroOffset(-36);
-  mpu.setZAccelOffset(1060); // 1688 factory default for my test chip
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
-    // turn on the DMP, now that it's ready
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    mpu.PrintActiveOffsets();
-    bleSerial.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
-    bleSerial.println(F("OK"));
-    acc_sensor_ready = true;
-    return;
-  } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    bleSerial.print(F("DMP Initialization failed (code "));
-    bleSerial.print(devStatus);
-    bleSerial.println(F(")"));
-  }
-
-failed:
-  enable_acc_sensor(false);
+  sensor_status();
+  delay(100);
+  sensor_cal_status();
+  sensor_ready = true;
 }
 
-void read_accelerator()
+void printEvent(sensors_event_t* event)
 {
-  int32_t delta;
-  uint8_t has_data;
-
-  has_data = mpu.dmpGetCurrentFIFOPacket(fifoBuffer);
-
-  if (!switch_state)
-    return;
-
-  if (!motion_monitor) {
-    motion_monitor = true;
-    motion_detect_timer = 0;
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+  if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION 
+      || event->type == SENSOR_TYPE_ACCELEROMETER
+      || event->type == SENSOR_TYPE_GRAVITY) {
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+  }
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+    x = event->magnetic.x;
+    y = event->magnetic.y;
+    z = event->magnetic.z;
+  }
+  else if ((event->type == SENSOR_TYPE_GYROSCOPE) || (event->type == SENSOR_TYPE_ROTATION_VECTOR)) {
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
   }
 
-  // read a packet from FIFO
-  if (has_data) { // Get the Latest packet 
-    // display real acceleration, adjusted to remove gravity
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetAccel(&aa, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+  if (x != 0 || y != 0 || z != 0) {
+    Serial.println();
+    Serial.print(event->type);
+    Serial.print(": x= ");
+    Serial.print(x);
+    Serial.print(" | y= ");
+    Serial.print(y);
+    Serial.print(" | z= ");
+    Serial.println(z);
+  }
+}
 
-    //Serial.print("aaReal.x\t");Serial.print(aaReal.x);Serial.println(F(""));
-    //Serial.print("aaWorld.x\t");Serial.print(aaWorld.x);Serial.println(F(""));
+void read_sensor()
+{
+  sensors_event_t orientationData;
+  double angle;
+  int32_t pressure;
 
-    if (motion_detect_timer == 0) {
-      if (abs(aaWorld.x) > MOTION_SENSITIVE) {
-        motion_detect_timer = millis();
-        acc_total = aaWorld.x;
-      }
-    } else {
-      if ((millis() - motion_detect_timer) > MOTION_DURATION_CHECK) {
-        motion_detect_timer = 0;
-        delta = ((float)acc_total / ((float)MOTION_DURATION_CHECK / MOTION_AVERAGE)) * WEIGHT_MOVING;
-        //Serial.print("acc_total\t");Serial.print(acc_total);Serial.println(F(""));
-        bleSerial.print(F("Delta\t"));bleSerial.print(delta);bleSerial.println(F(""));
-        delta = (float)delta * PRESSURE_RATIO;
-        if (acc_total > 0) {
-          bleSerial.println(F("Moving right: "));
-          if (last_move < 0) {
-            // Moved left last time, sudden move?
-            last_delta = delta;
-            delta = 0;
-          } else {
-            delta += last_delta;
-            last_delta = 0;
-          }
-          last_move = 1;
-        } else {
-          bleSerial.println(F("Moving left: "));
-          if (last_move > 0) {
-            // Moved right last time, sudden move?
-            last_delta = delta;
-            delta = 0;
-          }
-          else {
-            delta += last_delta;
-            last_delta = 0;
-          }
-          last_move = -1;
-        }
-        if (delta != 0) {
-          current_pressure -= delta;
-          if (current_pressure > PRESSURE_CUT_HIGH)
-            current_pressure = PRESSURE_CUT_HIGH;
-          if (delta > 0 && current_pressure < PRESSURE_CUT_LOW)
-            current_pressure = PRESSURE_CUT_LOW;
-          motor_analog_out(current_pressure);
-        }
-      } else {
-        acc_total += aaWorld.x;
-      }
+  if ((millis() - sensor_read_last_time) > BNO055_SAMPLERATE_DELAY_MS) {
+    sensor_read_last_time = millis();
+    bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+    angle = orientationData.orientation.x;
+    if (angle == 0 || !switch_state)
+      return;
+    bleSerial.print("Handle angle\t");bleSerial.println(angle);
+    if (angle < 0 || angle > ANGLE_NOISE_FILTER)
+      angle = ANGLE_CUT_LOW;
+    if (angle > ANGLE_CUT_HIGH)
+      angle = ANGLE_CUT_HIGH;
+    pressure = map(angle, ANGLE_CUT_LOW, ANGLE_CUT_HIGH, PRESSURE_CUT_LOW, PRESSURE_CUT_HIGH);
+    if (abs(pressure - current_pressure) > PRESSURE_MIN_STEP) {
+      current_pressure = pressure;
+      motor_analog_out(pressure);
     }
   }
 }
@@ -277,22 +230,19 @@ void motor_analog_out(int32_t pressure)
 
 void motor_defaut_speed()
 {
-  motor_analog_out(PRESSURE_CUT_LOW);
+  current_pressure = PRESSURE_CUT_HIGH;
+  motor_analog_out(PRESSURE_CUT_HIGH);
 }
 
 void setup() {
-  acc_total = 0;
-  last_delta = 0;
-  last_move = 0;
   current_pressure = 0;
   last_pressure = 0;
   refill_state = 0;
   brew_state = 0;
   switch_state = 0;
-  motion_monitor = false;
-  motion_detect_timer = 0;
+  sensor_ready = false;
+  sensor_read_last_time = 0;
 
-  pinMode(VCC_SENSOR_PIN, OUTPUT);
   // refill sensor
   pinMode(REFILL_PIN, INPUT);
   // brew sensor
@@ -313,8 +263,7 @@ void setup() {
     bleSerial.println(F("failed to initialize HardwareSerial!"));
   }
 
-  enable_acc_sensor(true);
-  setup_acc_sensor();
+  setup_sensor();
   wave.begin(1);
   WDT.begin(WDT_TIMEOUT);
   motor_defaut_speed();
@@ -335,6 +284,9 @@ void motor_enable(bool on)
 void loop() {
   WDT.refresh();
   BLE.poll();
+
+  if (sensor_ready)
+    read_sensor();
 
   if (digitalRead(BREW_PIN_IN) == LOW) {
     if (brew_state == 0) {
@@ -370,31 +322,14 @@ void loop() {
     if (switch_state == 0) {
       bleSerial.println(F("Switch pressed"));
       switch_state = 1;
-      current_pressure = START_MOVE_PRESSURE;
       motor_defaut_speed();
-      bleSerial.print(F("acc_sensor_ready\t")); bleSerial.print(acc_sensor_ready); bleSerial.println(F(""));
-      bleSerial.print(F("temp\t")); bleSerial.print(get_temp()); bleSerial.println(F(""));
+      bleSerial.print("sensor_ready\t");bleSerial.println(sensor_ready);
     }
   } else {
     if (switch_state == 1) {
       bleSerial.println(F("Switch de-pressed"));
-      motion_monitor = false;
       switch_state = 0;
       motor_enable(false);
-      bleSerial.print(F("acc_sensor_enable_timer\t")); bleSerial.print(acc_sensor_enable_timer); bleSerial.println(F(""));
-      bleSerial.print(F("millis\t")); bleSerial.print(millis()); bleSerial.println(F(""));
-      if (!acc_sensor_ready || (get_temp() <= 37)) {
-        resetFunc();
-      }
-    }
-  }
-
-  if (acc_sensor_ready) {
-    read_accelerator();
-
-    if (abs(millis() - acc_sensor_enable_timer) > ACC_SENSOR_LIFE_DURATION) {
-      bleSerial.println(F("Turn off sensor"));
-      enable_acc_sensor(false);
     }
   }
 
