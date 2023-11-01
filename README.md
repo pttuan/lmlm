c52f8b226a9df9edf00d81274610ab601dd74091
Tuan Phan <pttuan@gmail.com>
2023-11-01 11:05:32 -0700

    Support brew-by-weight with Lunar 2021
    Signed-off-by: Tuan Phan <pttuan@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # lmlm
+Hardwares:
+- Arduino Uno R4 wifi
+- BNO055 breakout board
+- DC motor supports control RPM by voltage
+- Input signal that captures the active state of LMLM's relay: brew, refill, paddle switch on
+
+Software:
+- Tested on Arduino IDE
+  + Requirements libraries: ArduinoBLE
--- lmlm.ino
+++ lmlm.ino
@@ -28,7 +28,7 @@ THE SOFTWARE.
 #include "Adafruit_BNO055.h"
 #include <WDT.h>
 #include <ArduinoBLE.h>
-#include <HardwareBLESerial.h>
+#include "lunarGateway.h"
 
 #define WDT_TIMEOUT               5000
 
@@ -37,10 +37,19 @@ THE SOFTWARE.
 #define PRESSURE_CUT_HIGH         9000
 #define PRESSURE_VOLTAGE_TOP      40000
 #define PRESSURE_VOLTAGE_BOTTOM   65000
-#define PRESSURE_MIN_STEP         100
 #define ANGLE_CUT_LOW             0
 #define ANGLE_CUT_HIGH            50
 #define ANGLE_NOISE_FILTER        100
+#define ANGLE_MIN_STEP            3
+#define LUNAR_ACTIVE_ANGLE        5
+#define LUNAR_WEIGH_RAMUP         3
+#define LUNAR_RAMUP_PRESSURE_STEP 300
+#define LUNAR_RAMUP_TIME_INTERVAL 100
+#define LUNAR_WEIGH_RAMDOWN       25
+#define LUNAR_RAMDOWN_PRESSURE_STEP 100
+#define LUNAR_RAMDOWN_TIME_INTERVAL 200
+
+#define LUNAR_WEIGH_STOP          35
 
 #define REFILL_PIN                D2
 #define BREW_PIN_IN               D3
@@ -49,12 +58,14 @@ THE SOFTWARE.
 #define SWITCH_IN                 D6
 
 #define BNO055_SAMPLERATE_DELAY_MS  50 //how often to read data from the board
+#define SCALE_SAMPLERATE_DELAY_MS  100 //how often to read data from the scale
 
+bool ble_active;
+
+// paddle sensor
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29, &Wire1);
-
-HardwareBLESerial &bleSerial = HardwareBLESerial::getInstance();
-
 bool sensor_ready;
+double last_angle;
 unsigned long sensor_read_last_time;
 
 // DAC vars
@@ -70,8 +81,35 @@ int refill_state;
 int brew_state;
 int switch_state;
 
-void motor_analog_out(int32_t pressure);
+// Scale
+lunarGateway lunar;
+bool found_lunar;
+bool lunar_active;
+bool lunar_session_init;
+int lunar_state;
+bool lunar_start_timer;
+unsigned long scale_read_last_time;
+unsigned long auto_ramup_last_time;
+unsigned long auto_ramdown_last_time;
+
+void motor_set_pressure(int32_t pressure);
 void(* resetFunc) (void) = 0;
+
+void ble_discover_lunar()
+{
+  BLEDevice peripheral = BLE.available();
+  // print the local name, if present
+  if (peripheral && peripheral.hasLocalName()) {
+    if (peripheral.localName().indexOf("LUNAR-") == 0) {
+      if (lunar.connect(&peripheral)) {
+        Serial.println("Found LUNAR device");
+        found_lunar = true;
+        scale_read_last_time = 0;
+        BLE.stopScan();
+      }
+    }
+  }
+}
 
 void sensor_status(void)
 {
@@ -81,16 +119,16 @@ void sensor_status(void)
   bno.getSystemStatus(&system_status, &self_test_results, &system_error);
 
   /* Display the results in the Serial Monitor */
-  bleSerial.println("");
-  bleSerial.print("Current mode: 0x");
-  bleSerial.println(bno.getMode(), HEX);
-  bleSerial.print("System Status: 0x");
-  bleSerial.println(system_status, HEX);
-  bleSerial.print("Self Test:     0x");
-  bleSerial.println(self_test_results, HEX);
-  bleSerial.print("System Error:  0x");
-  bleSerial.println(system_error, HEX);
-  bleSerial.println("");
+  Serial.println("");
+  Serial.print("Current mode: 0x");
+  Serial.println(bno.getMode(), HEX);
+  Serial.print("System Status: 0x");
+  Serial.println(system_status, HEX);
+  Serial.print("Self Test:     0x");
+  Serial.println(self_test_results, HEX);
+  Serial.print("System Error:  0x");
+  Serial.println(system_error, HEX);
+  Serial.println("");
 }
 
 void sensor_cal_status(void)
@@ -105,18 +143,18 @@ void sensor_cal_status(void)
   /* The data should be ignored until the system calibration is > 0 */
   if (!system)
   {
-    bleSerial.print("! ");
+    Serial.print("! ");
   }
 
   /* Display the individual values */
-  bleSerial.print("Sys:");
-  bleSerial.print(system, DEC);
-  bleSerial.print(" G:");
-  bleSerial.print(gyro, DEC);
-  bleSerial.print(" A:");
-  bleSerial.print(accel, DEC);
-  bleSerial.print(" M:");
-  bleSerial.println(mag, DEC);
+  Serial.print("Sys:");
+  Serial.print(system, DEC);
+  Serial.print(" G:");
+  Serial.print(gyro, DEC);
+  Serial.print(" A:");
+  Serial.print(accel, DEC);
+  Serial.print(" M:");
+  Serial.println(mag, DEC);
 }
 
 void setup_sensor()
@@ -126,7 +164,7 @@ void setup_sensor()
 
   if (!bno.begin(OPERATION_MODE_IMUPLUS))
   {
-    bleSerial.print("No BNO055 detected");
+    Serial.print("No BNO055 detected");
     return;
   }
   Wire1.setClock(400000);
@@ -178,46 +216,64 @@ void printEvent(sensors_event_t* event)
   }
 }
 
+void read_scale()
+{
+  if ((millis() - scale_read_last_time) > SCALE_SAMPLERATE_DELAY_MS) {
+    lunar.read();
+  }
+
+  lunar.sendHeartBeat();
+}
+
 void read_sensor()
 {
   sensors_event_t orientationData;
-  double angle;
   int32_t pressure;
+  double angle;
 
   if ((millis() - sensor_read_last_time) > BNO055_SAMPLERATE_DELAY_MS) {
     sensor_read_last_time = millis();
     bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
     angle = orientationData.orientation.x;
-    if (angle == 0 || !switch_state)
+    if (angle == 0)
       return;
-    bleSerial.print("Handle angle\t");bleSerial.println(angle);
     if (angle < 0 || angle > ANGLE_NOISE_FILTER)
       angle = ANGLE_CUT_LOW;
     if (angle > ANGLE_CUT_HIGH)
       angle = ANGLE_CUT_HIGH;
-    pressure = map(angle, ANGLE_CUT_LOW, ANGLE_CUT_HIGH, PRESSURE_CUT_LOW, PRESSURE_CUT_HIGH);
-    if (abs(pressure - current_pressure) > PRESSURE_MIN_STEP) {
-      current_pressure = pressure;
-      motor_analog_out(pressure);
-    }
+    if (abs(angle - last_angle) > ANGLE_MIN_STEP)
+      last_angle = angle;
+    else
+      return;
+    pressure = map(last_angle, ANGLE_CUT_LOW, ANGLE_CUT_HIGH, PRESSURE_CUT_LOW, PRESSURE_CUT_HIGH);
+    motor_set_pressure(pressure);
   }
 }
 
-void motor_analog_out(int32_t pressure)
+void motor_enable(bool on)
 {
-  bleSerial.print(F("Motor set pressure\t"));
-  bleSerial.print(pressure);
-  bleSerial.println(F(""));
+  if (on) {
+    digitalWrite(MOTOR_ENABLE_OUT, HIGH);
+  } else {
+    digitalWrite(MOTOR_ENABLE_OUT, LOW);
+  }
+}
 
+void motor_set_pressure(int32_t pressure)
+{
   if (pressure < PRESSURE_CUT_LOW)
     pressure = PRESSURE_CUT_LOW;
   if (pressure > PRESSURE_CUT_HIGH)
     pressure = PRESSURE_CUT_HIGH;
-
+  current_pressure = pressure;
   last_pressure = map(dac_sample[0],
                               PRESSURE_VOLTAGE_TOP, PRESSURE_VOLTAGE_BOTTOM,
                               PRESSURE_CUT_LOW, PRESSURE_CUT_HIGH);
   last_pressure = PRESSURE_CUT_HIGH - (last_pressure - PRESSURE_CUT_LOW);
+  Serial.print(F("Motor set pressure: "));Serial.println(pressure);
+  Serial.print(F("Last pressure: "));Serial.println(last_pressure);
+  if (pressure == last_pressure)
+    return;
 
   for (int i = 0; i < sizeof(dac_sample) / sizeof(dac_sample[0]); i++) {
     dac_sample[i] = (uint16_t)map(pressure,
@@ -228,20 +284,31 @@ void motor_analog_out(int32_t pressure)
   wave.offset(0);
 }
 
-void motor_defaut_speed()
+void motor_defaut_pressure()
 {
-  current_pressure = PRESSURE_CUT_HIGH;
-  motor_analog_out(PRESSURE_CUT_HIGH);
+  motor_set_pressure(PRESSURE_CUT_HIGH);
+}
+
+void lunar_auto_ramup()
+{
+  if (millis() - auto_ramup_last_time > LUNAR_RAMUP_TIME_INTERVAL) {
+    auto_ramup_last_time = millis();
+    motor_set_pressure(current_pressure + LUNAR_RAMUP_PRESSURE_STEP);
+  }
+}
+
+void lunar_auto_ramdown()
+{
+  if (millis() - auto_ramdown_last_time > LUNAR_RAMDOWN_TIME_INTERVAL) {
+    auto_ramdown_last_time = millis();
+    motor_set_pressure(current_pressure - LUNAR_RAMDOWN_PRESSURE_STEP);
+  }
 }
 
 void setup() {
-  current_pressure = 0;
-  last_pressure = 0;
   refill_state = 0;
   brew_state = 0;
   switch_state = 0;
-  sensor_ready = false;
-  sensor_read_last_time = 0;
 
   // refill sensor
   pinMode(REFILL_PIN, INPUT);
@@ -259,77 +326,134 @@ void setup() {
   // initialize serial communication
   Serial.begin(115200);
 
-  if (!bleSerial.beginAndSetupBLE("SerialPassthrough")) {
-    bleSerial.println(F("failed to initialize HardwareSerial!"));
+  // begin initialization
+  found_lunar = false;
+  ble_active = false;
+  if (BLE.begin()) {
+    ble_active = true;
+    BLE.scan();
+  } else {
+    Serial.println("Failed to begin BLE");
   }
 
   setup_sensor();
   wave.begin(1);
   WDT.begin(WDT_TIMEOUT);
-  motor_defaut_speed();
+  motor_defaut_pressure();
 }
 
-void motor_enable(bool on)
-{
-  if (on) {
-    digitalWrite(MOTOR_ENABLE_OUT, HIGH);
-  } else {
-    digitalWrite(MOTOR_ENABLE_OUT, LOW);
-  }
-}
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
 
 void loop() {
   WDT.refresh();
-  BLE.poll();
 
   if (sensor_ready && switch_state == 1)
     read_sensor();
 
+  if (ble_active) {
+    BLE.poll();
+
+    if (found_lunar) {
+      if (lunar.connected()) {
+        read_scale();
+        if (lunar_active && (!sensor_ready || last_angle < LUNAR_ACTIVE_ANGLE)) {
+          if (!lunar_session_init) {
+            Serial.println("Lunar session started");
+            lunar_session_init = true;
+            auto_ramup_last_time = 0;
+            auto_ramdown_last_time = 0;
+            lunar_start_timer = false;
+            lunar_state = 0;
+            lunar.stopTimer();
+            lunar.doTare();          
+          } else {
+            switch (lunar_state) {
+            case 0:
+              if (lunar.weight > LUNAR_WEIGH_RAMUP && lunar.weight <= LUNAR_WEIGH_RAMDOWN) {
+                lunar_state++;
+                if (!lunar_start_timer) {
+                  Serial.println("Lunar timer started");
+                  lunar.startTimer();
+                }
+                Serial.println("Lunar ramup");
+              }
+              break;
+            case 1:
+              lunar_auto_ramup();
+              if (lunar.weight > LUNAR_WEIGH_RAMDOWN && lunar.weight <= LUNAR_WEIGH_STOP) {
+                lunar_state++;
+                Serial.println("Lunar ramdown");
+              }
+              break;
+            case 2:
+              lunar_auto_ramdown();
+              if (lunar.weight > LUNAR_WEIGH_STOP) {
+                lunar_state++;
+                Serial.println("Lunar stopped");
+                motor_enable(false);
+                lunar.stopTimer();
+                lunar_active = false;
+              }
+              break;
+            }
+          }
+        }
+      } else if (!lunar_active) {
+        BLE.scan();
+        found_lunar = false;
+      }
+    } else {
+      if (!lunar_active)
+        ble_discover_lunar();
+    }
+  }
 
   if (digitalRead(BREW_PIN_IN) == LOW) {
     if (brew_state == 0) {
-      bleSerial.println(F("BREW started"));
+      Serial.println(F("BREW started"));
       brew_state = 1;
       motor_enable(true);
       delay(100);
     }
   } else {
     if (brew_state == 1) {
-      bleSerial.println(F("BREW stopped"));
+      Serial.println(F("BREW stopped"));
       brew_state = 0;
       motor_enable(false);
     }
   }
   if (digitalRead(REFILL_PIN) == LOW) {
     if (refill_state == 0) {
-      bleSerial.println(F("REFILL started"));
+      Serial.println(F("REFILL started"));
       refill_state = 1;
-      motor_analog_out(PRESSURE_REFILL);
+      motor_set_pressure(PRESSURE_REFILL);
       motor_enable(true);
     }
   } else {
     if (refill_state == 1) {
-      bleSerial.println(F("REFILL stopped"));
+      Serial.println(F("REFILL stopped"));
       refill_state = 0;
-      motor_analog_out(last_pressure);
+      motor_set_pressure(last_pressure);
       motor_enable(false);
     }
   }
 
   if (digitalRead(SWITCH_IN) == LOW) {
     if (switch_state == 0) {
-      bleSerial.println(F("Switch pressed"));
+      Serial.println(F("Switch pressed"));
       switch_state = 1;
-      motor_defaut_speed();
-      bleSerial.print("sensor_ready\t");bleSerial.println(sensor_ready);
+      last_angle = 0;
+      lunar_active = true;
+      lunar_session_init = false;
+      motor_defaut_pressure();
     }
   } else {
     if (switch_state == 1) {
-      bleSerial.println(F("Switch de-pressed"));
+      Serial.println(F("Switch de-pressed"));
       switch_state = 0;
+      lunar_active = false;
       motor_enable(false);
     }
   }
